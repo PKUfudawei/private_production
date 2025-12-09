b4d3ed16d70a3fd7d1100d693c9a3e67a882a986
Dawei Fu <fudw@pku.edu.cn>
2025-12-09 19:32:24 +0800

    update

--- main.sh
+++ main.sh
@@ -2,7 +2,10 @@
 
 source ./wmLHEGEN.sh $1
 source ./SIM.sh $1
-source ./DIGIPremix.sh $1
+for i in {1..5}; do
+    source ./DIGIPremix.sh $1 && break
+    sleep 10
+done
 source ./HLT.sh $1
 source ./RECO.sh $1
 source ./MiniAODv2.sh $1
